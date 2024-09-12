d1274049030077f4c9de369d6db959ff8253881c
Javier Flores <javierflores55946@gmail.com>
2024-09-12 03:15:59 -0300

    Instalar paquete cookie

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DdWk6CTy.mjs';
-import { manifest } from './manifest_CPPPQYsj.mjs';
+import { manifest } from './manifest_B9tWYfIv.mjs';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -37,7 +37,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "341b5f58-cd93-4373-a65d-0b1012687370",
+    "middlewareSecret": "916e5971-2ae1-44e4-b0f9-46db3cbf986f",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
