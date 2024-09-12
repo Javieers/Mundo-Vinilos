37e260a5fe1eb3c58c3d5f397568d384cf6686e8
Javier Flores <javierflores55946@gmail.com>
2024-09-12 02:42:31 -0300

    Agregar adaptador de Vercel2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,26 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   server: {
     fs: {
       allow: ['src']
     }
   },
+
   site: 'https://lexingtonthemes.com',
-  integrations: [sitemap()], // Elimina db() de aquí
-  output: 'server' // Asegúrate de que esto esté configurado como 'server' o 'hybrid'
+
+  // Elimina db() de aquí
+  integrations: [sitemap()],
+
+  // Asegúrate de que esto esté configurado como 'server' o 'hybrid'
+  output: 'server',
+
+  adapter: vercel()
 });
